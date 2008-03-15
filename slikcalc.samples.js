2bee51149998904471c7b98521a593b4546f3c8a
Brad Harris <bmharris@gmail.com>
2008-03-15 04:55:56 +0000

    updates to site page

---
+++ slikcalc.samples.js
@@ -0,0 +1,45 @@
+(function() {
+    headerCalc1 = new slikcalc.FormulaCalc({
+        formula: '( {a} / {b} ) + {c} = {d}',
+		vars: {
+			a: { id: 'header-1-1' },
+			b: { id: 'header-1-2' },
+			c: { id: 'header-1-3'},
+			d: { id: 'header-1-total'}
+		},
+		registerListeners: true,
+		calcOnLoad: true
+    });
+    headerCalc2 = new slikcalc.FormulaCalc({
+        formula: '( {a} * {b} ) + {c} = {d}',
+		vars: {
+			a: { id: 'header-2-1' },
+			b: { id: 'header-2-2' },
+			c: { id: 'header-2-3'},
+			d: { id: 'header-2-total'}
+		},
+		registerListeners: true,
+		calcOnLoad: true
+    });
+    headerTotalsCalc = new slikcalc.FormulaCalc({
+       formula: '{a} + {b} = {c}',
+       vars: {
+           a: { id: 'header-1-total' },
+           b: { id: 'header-2-total' },
+           c: { id: 'header-totals' }
+       }
+    });
+    headerCalc1.triggers(headerTotalsCalc);
+    headerCalc2.triggers(headerTotalsCalc);
+    
+    
+    var columnCalc1 = new slikcalc.ColumnCalc({
+		totalId: 'cc-1-total',
+		registerListeners: true,
+		calcOnLoad: true
+	});
+	columnCalc1.addRow({ id: 'cc-1-1' });
+	columnCalc1.addRow({ id: 'cc-1-2' });
+	columnCalc1.addRow({ id: 'cc-1-3' });
+	
+})();
